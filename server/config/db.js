7f78026704954e61a6d0cc75e87b9fe3a1fc0515
kendell <kenren@udel.edu>
2024-06-05 02:08:16 -0400

    made mongoose deploy connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/FitX')
+
+//remember to change this back to it truthy 
+if (process.env.PORT) {
+    const mongoUri = process.env.MONGODB_URI;
+    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB connected...'))
+        .catch(err => console.log('MongoDB connection error:', err));
+} else {
+    mongoose.connect('mongodb://127.0.0.1:27017/FitX')
+}
 
 module.exports = mongoose.connection
